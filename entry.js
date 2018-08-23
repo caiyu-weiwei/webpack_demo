0f4f65b0ffcf96fbb5a249fcd0e17d2dc4193b29
caiweiwei <caiweiwei@yazuo.com>
2018-08-23 17:08:49 +0800

    webpack.ProvidePlugin应用

--- entry.js
+++ entry.js
@@ -2,6 +2,6 @@ import css from './css/index.css'
 import less from './css/black.less'
 import sass from './css/buty.scss'
 import caiweiwei from './caiweiwei.js'
-import $ from 'jquery'
+// import $ from 'jquery'
 document.getElementById("title").innerHTML = "HELLO CAI WEI WEI!"
 caiweiwei()
